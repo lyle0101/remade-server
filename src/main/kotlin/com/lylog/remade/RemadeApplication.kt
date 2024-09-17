60bdb028dd728b011dc676bacee3e7a713299b0a
Lyle Son <calends99@gmail.com>
2024-09-17 15:19:14 +0900

    CHORE: 프로젝트 세팅

---
+++ src/main/kotlin/com/lylog/remade/RemadeApplication.kt
@@ -0,0 +1,11 @@
+package com.lylog.remade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RemadeApplication
+
+fun main(args: Array<String>) {
+	runApplication<RemadeApplication>(*args)
+}
---
+++ src/test/kotlin/com/lylog/remade/RemadeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lylog.remade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemadeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
